d25489cf1701dabedd466c3b4967045318b94b80
TKobina <thatcher.kobi@gmail.com>
2024-01-14 17:20:37 -0600

    framework for generating mock data built

--- lib/seeder_class.rb
+++ lib/seeder_class.rb
@@ -1,30 +1,75 @@
 class SeederClass
-  def data
-    @mock_data
-  end
 
-  def initialize(data_file)
-    @mock_data = YAML.load_file("#{Rails.root.to_s}" + data_file)
+  def data
+    #@mock_data
   end
 
   def unseed
-    #Get all mock user id's, store them in array
-    mock_users_id = []
-    @mock_data['users'].each { |x| mock_users_id << User.find_by(email: x['email']).id if User.where(email: x['email']).exists? }
+    mock_user_ids = read_mock_user_ids
 
-    #Destroy all records belonging to each mock user, then destroy all mock users
-    mock_users_id.each { |x| Entity.where(user_id: x).destroy_all }
-    mock_users_id.each { |x| Event.where(user_id: x).destroy_all }
-    mock_users_id.each { |x| User.find(x).destroy }
+    mock_user_ids.each { |x| Entity.where(user_id: x).destroy_all }
+    mock_user_ids.each { |x| Event.where(user_id: x).destroy_all }
+    mock_user_ids.each { |x| User.find(x).destroy }
+
+    write_mock_user_ids([])
   end
 
-  def seed
-    #Seed mock data
-    #Create Users
-    User.create!(@mock_data["users"])
+  def seed_users(n)
+    base_email = Rails.application.credentials.mock_seeding_email
+    mock_password = Rails.application.credentials.mock_password
+    
+    mock_user_ids = []
 
-    #Get id's for newly created users
-    mock_users_id = []
-    @mock_data['users'].each { |x| mock_users_id << User.find_by(email: x['email']).id if User.where(email: x['email']).exists? }
+    begin
+      n.times do      
+        mock_user_ids << User.create!(
+          email: base_email + "+" + Faker::Name.first_name + Time.now.to_i.to_s + "@gmail.com",
+          password: mock_password,
+          password_confirmation: mock_password,
+          privileged: true
+        ).id
+      end
+    rescue
+      puts "SeederClass::seed_users broke, for some reason; ids for mock users already generated:"
+      puts mock_user_ids
+    end
+
+    append_mock_user_ids(mock_user_ids)
+  end
+
+  def seed_entities(n)
+    n.times do
+      #Entity.create!(generate_faker_entity)
+    end
+  end
+
+  def seed_events(n)
+    n.times do
+      
+    end
+  end
+
+  private
+
+  def append_mock_user_ids(mock_user_ids)
+    extant_ids = read_mock_user_ids
+    write_mock_user_ids((mock_user_ids + extant_ids).uniq)
+  end
+
+  def write_mock_user_ids(mock_user_ids)
+    titled_ids = { :title => "mock_user_ids", user_ids: mock_user_ids}
+    File.open("#{Rails.root.to_s}/db/seeds/mock_data.yml", "w") { |file| file.write(titled_ids.to_yaml) }
+  end
+
+  def read_mock_user_ids
+    YAML.load_file("#{Rails.root.to_s}/db/seeds/mock_data.yml")[:user_ids]
+  end
+
+  def generate_faker_entity
+
+  end
+
+  def generate_faker_event
+
   end
 end
